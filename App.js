b4106b017bf24d960e3b0fbe3b3fb06b63f6c1ca
Ruairi Griffin <ruairi.griffin@atomic.io>
2022-11-07 11:24:22 +1300

    Renames variables to match web sdk example

--- App.js
+++ App.js
@@ -1,39 +1,36 @@
 /**
- * Boilerplate React Native App For Atomic SDK
- */
+* Boilerplate React Native App For Atomic SDK
+*/
 
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { Session as AtomicSession } from "@atomic.io/react-native-atomic-sdk";
-import { StreamContainer } from "@atomic.io/react-native-atomic-sdk";
+import { Session as AtomicSession, StreamContainer } from "@atomic.io/react-native-atomic-sdk";
 
-const Credentials = {
-  containerId: "***REMOVED***",
-  apiHost: "***REMOVED***",
-  apiKey: "***REMOVED***",
-  environmentId: "***REMOVED***",
+const ATOMIC_API_HOST = "***REMOVED***";
+const ATOMIC_API_KEY = "***REMOVED***";
+const ATOMIC_ENVIRONMENT_ID = "***REMOVED***";
+const ATOMIC_STREAM_CONTAINER_ID = "***REMOVED***";
+const ATOMIC_REQUEST_TOKEN_STRING = "***REMOVED***"
 
-  requestTokenStr: "***REMOVED***",
-}
+const onAuthTokenRequested = async () => {
+  // This function will called by the Atomic SDK to authenticate a user.
+  // You would normally get this value from your authentication process.
+  // For this example we will just return a hardcoded string.
+  return ATOMIC_REQUEST_TOKEN_STRING
+};
 
-const InitialiseAtomic = () => {
-  AtomicSession.initialise(Credentials.environmentId, Credentials.apiKey);
-  AtomicSession.setApiBaseUrl(Credentials.apiHost);
-  AtomicSession.setSessionDelegate(async () => {
-    return Credentials.requestTokenStr
-  })
-}
-
-InitialiseAtomic()
+AtomicSession.initialise(ATOMIC_ENVIRONMENT_ID, ATOMIC_API_KEY);
+AtomicSession.setApiBaseUrl(ATOMIC_API_HOST);
+AtomicSession.setSessionDelegate(onAuthTokenRequested)
 
 const App = () => {
   return (
     <SafeAreaView>
       <StreamContainer
         style={{ width: '100%', height: '100%' }}
-        containerId={Credentials.containerId}
+        containerId={ATOMIC_STREAM_CONTAINER_ID}
         configuration={{}}
-      />
+        />
     </SafeAreaView>
   );
 };
